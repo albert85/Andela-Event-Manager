23835bf95535406e79f805ec89ecbd5a6f346ccd
albert85 <tmtpalbert@gmail.com>
2017-11-23 18:46:22 +0100

    creating a full event manager database

--- server/api/controller/eventclass.js
+++ server/api/controller/eventclass.js
@@ -1,5 +1,3 @@
-// importing express lib
-import express from 'express';
 
 // import database
 import eventDatabase from '../model/database.js';
@@ -7,15 +5,91 @@ import eventDatabase from '../model/database.js';
 
 export default class Eventmanager{
 
-	
-	static getNewEvent(req, resp){
+	// get all events
+	static getAllEvent(req, resp){
 		
-		return resp.json({ users: eventDatabase });
+		return resp.json({ result: eventDatabase, message: "success", error: false });
 	}
 
-	// check for the validity of the parameter supplied
-	static checkValidity(req, resp, next){
-		// if()
+	// Create an event
+	static addNewEvent(req, resp){
+		if(!req.body.name || !req.body.location|| !req.body.startTime || !req.body.endTime){
+			return resp.json({status: "error", 
+			message: "Please fill all field"})
+		}
+
+		// creating a new event
+		const newEvent = {
+			id: eventDatabase.user.length,
+			name: req.body.name,
+			location: req.body.location,
+			startTime: req.body.startTime,
+			endTime: req.body.endTime
+		}
+		eventDatabase.user.push(newEvent);
+		resp.json({
+			message: 'New event was created',
+			Error: false
+		})
+
 	}
+
+	// Edit an event with a user id
+	static editAnEvent(req,resp){
+		// check if the validity of the input
+		if (!req.body.name || !req.body.location || !req.body.startTime || !req.body.endTime) {
+			return resp.json({
+				message: 'Please supply name, location startTime and endTime of the event',
+				Error: true
+			})
+		}
+		// Update the event using event id
+		for (x of eventDatabase.user) {
+			if (x.id === parseInt(req.params.eventid, 10)) {
+				eventDatabase.user[req.params.eventid].name = req.body.name;
+				eventDatabase.user[req.params.eventid].location = req.body.location;
+				eventDatabase.user[req.params.eventid].startTime = req.body.startTime;
+				eventDatabase.user[req.params.eventid].endTime = req.body.endTime;
+
+				return resp.json({
+					Message: `User ${x.id} updated`,
+					error: false,
+					user: eventDatabase.user[eventid]
+
+				})
+			}
+		}
+
+		return resp.status(404).json({
+			status: 'User not found',
+			error: true
+		})
+	}
+
+	static deleteAnEvent(req, resp){
+		for (val of eventDatabase.user){
+			if (val.id === parseInt(req.params.eventid, 10)) {
+				events.splice(val.id, 1);
+
+				//Updating the id in the database
+				let count = 0;
+				for (val of eventDatabase.user) {
+					events[count].id = count;
+					count++;
+				}
+				return resp.json({
+					Message: `User deleted`,
+					Error: false
+				})
+			}
+		}
+
+		return resp.json({
+			Message: 'User id not found',
+			Error: true
+		})
+
+	}
+
 
 }
--- server/api/router/eventrouter.js
+++ server/api/router/eventrouter.js
@@ -1,8 +1,6 @@
 // importing express lib
 import express from 'express'
 
-import eventDatabase from '../model/database.js';
-
 // importing eventclas
 import eventclassrouter from '../controller/eventclass.js'
 
@@ -10,11 +8,16 @@ import eventclassrouter from '../controller/eventclass.js'
 const eventrouter = express.Router();
 
 eventrouter.route('/')
-    .get(eventclassrouter.getNewEvent)
-// get all events
-// eventrouter.route('/')
-// // .all()
-//     .get(eventclassrouter.getNewEvent)
-
+    // get all events
+    .get(eventclassrouter.getAllEvent)
+    // create a new event
+    .post(eventclassrouter.addNewEvent)
+    //check if an id to be deleted is supplied
+    .delete((req,resp)=>{
+        resp.json({
+            Message: 'Id not supplied',
+            Error: true
+        })
+    })
 
 export default eventrouter;
