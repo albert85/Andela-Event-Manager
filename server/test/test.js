5e185cbe0552407ae979fa13c6da2a17df38ca1b
albert85 <tmtpalbert@gmail.com>
2017-11-24 20:13:38 +0100

    testing with mocha

--- server/test/test.js
+++ server/test/test.js
@@ -1,19 +1,28 @@
 import chaihhtp from 'chai-http'
-import server from '../api/app'
+import server from '../api/app.js'
 import chai from 'chai'
 
-const {expect } = chai
+const {expect } = chai;
+
+chai.use(chaihhtp);
 
 describe('user events  database',()=>{
-    const data = {
+    const data = [{
         id: 0,
         name: "Appolan",
         location: "ikeja",
         endTime: 1,
         startTime: 2
-    }
-})
+    }]
+});
 
 describe('valid function',()=>{
-    it('')
-})
+    it('/getEvent',(done)=>{
+        chai.request(server)
+            .get('/api/v1/users/events')
+        .end((err,res)=>{
+            expect(res.body[0].id).to.be.eql(data[0].id);
+            done();
+        })
+    });
+});
