01495578e52a051d71082f96be5ae909d6383659
albert85 <tmtalbert@gmail.com>
2017-11-28 10:06:50 +0100

    setup documentation

--- server/api/app.js
+++ server/api/app.js
@@ -1,6 +1,8 @@
 // importing express and body-parser library
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
 
 // importing event and center route
 import routecontroller from './index';
@@ -9,17 +11,41 @@ import routecontroller from './index';
 const app = express();
 
 
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition: {
+    info: {
+      title: 'Node Swagger API',
+      version: '1.0.0',
+      description: 'Demonstrating how to describe a RESTful API with Swagger',
+    },
+    host: 'localhost:9098',
+    basePath: '/',
+  },
+  // path to the API docs
+  apis: ['./controller/*.js']
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
 // configuring body-parser to json property
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// serve swagger
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(options));
 // configuring the event and center route
 app.use(routecontroller);
-// app.use('/api/v1/users/events',routecontroller)
-// app.use('/api/v1/admin/centers',routecontroller)
 
 // listening to server at port localhost:8090
-app.listen(8090, () => {
+app.listen(9098, () => {
   console.log('server listening');
 });
 
--- server/api/controller/eventclass.js
+++ server/api/controller/eventclass.js
@@ -2,10 +2,8 @@
 // import database
 import eventDatabase from '../model/database.js';
 
-
 export default class Eventmanager {
-
-	/**
+ /**
   * @swagger
   * /api/v1/events:
   *   post:
@@ -68,89 +66,83 @@ export default class Eventmanager {
   *           }
   *       409:
   *         description: When the username is already in use
-	*/
+*/
 
-	// Create an event
-	static addNewEvent(req, res) {
-		if (!req.body.name || !req.body.location || !req.body.startTime || !req.body.endTime) {
-			return res.json({
-				status: "error",
-				message: "Please fill all field"
-			})
-		}
+  // Create an event
+  static addNewEvent(req, res) {
+    if (!req.body.name || !req.body.location || !req.body.startTime || !req.body.endTime) {
+      return res.json({
+        status: 'error',
+        message: 'Please fill all field',
+      });
+    }
 
-		// creating a new event
-		const newEvent = {
-			id: eventDatabase.events.length,
-			name: req.body.name,
-			location: req.body.location,
-			startTime: req.body.startTime,
-			endTime: req.body.endTime,
-			eventDate: req.body.eventDate
-		}
-		eventDatabase.events.push(newEvent);
+    // creating a new event
+    const newEvent = {
+      id: eventDatabase.events.length,
+      name: req.body.name,
+      location: req.body.location,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      eventDate: req.body.eventDate,
+    };
+    eventDatabase.events.push(newEvent);
 
-		res.json({
-			message: 'New event was created',
-			Error: false,
-			output: eventDatabase.events[eventDatabase.events.length - 1]
-		})
+    res.json({
+      message: 'New event was created',
+      Error: false,
+      output: eventDatabase.events[eventDatabase.events.length - 1],
+    });
+  }
 
-	}
+  // Edit an event with a user id
+  static editAnEvent(req, res) {
+    // check if the validity of the input
+    if (!req.body.name || !req.body.location || !req.body.startTime || !req.body.endTime) {
+      return res.json({
+        message: 'Please supply name, location startTime and endTime of the event',
+        Error: true,
+      });
+    }
+    // Update the event using event id
+    for (const val of eventDatabase.events) {
+      if (val.id === parseInt(req.params.eventid, 10)) {
+        eventDatabase.events[req.params.eventid].name = req.body.name;
+        eventDatabase.events[req.params.eventid].location = req.body.location;
+        eventDatabase.events[req.params.eventid].startTime = req.body.startTime;
+        eventDatabase.events[req.params.eventid].endTime = req.body.endTime;
+        eventDatabase.events[req.params.eventid].eventDate = req.body.eventDate;
+        return res.status(200).json({
+          Message: `User ${val.id} updated`,
+          error: false,
+          events: eventDatabase.events[req.params.eventid],
 
-	// Edit an event with a user id
-	static editAnEvent(req, res) {
-		// check if the validity of the input
-		if (!req.body.name || !req.body.location || !req.body.startTime || !req.body.endTime) {
-			return res.json({
-				message: 'Please supply name, location startTime and endTime of the event',
-				Error: true
-			})
-		}
-		// Update the event using event id
-		for (let val of eventDatabase.events) {
-			if (val.id === parseInt(req.params.eventid, 10)) {
-				eventDatabase.events[req.params.eventid].name = req.body.name;
-				eventDatabase.events[req.params.eventid].location = req.body.location;
-				eventDatabase.events[req.params.eventid].startTime = req.body.startTime;
-				eventDatabase.events[req.params.eventid].endTime = req.body.endTime;
-				eventDatabase.events[req.params.eventid].eventDate = req.body.eventDate;
-				return res.status(200).json({
-					Message: `User ${val.id} updated`,
-					error: false,
-					events: eventDatabase.events[req.params.eventid]
+        });
+      }
+    }
 
-				})
-			}
-		}
+    return res.status(409).json({
+      status: 'User not found',
+      error: true,
+    });
+  }
+  // delete an event
+  static deleteAnEvent(req, res) {
+    for (const val of eventDatabase.events) {
+      if (val.id === parseInt(req.params.eventid, 10)) {
+        eventDatabase.events.splice(val.id, 1);
 
-		return res.status(409).json({
-			status: 'User not found',
-			error: true
-		})
-	}
-	// delete an event
-	static deleteAnEvent(req, res) {
-		for (let val of eventDatabase.events) {
-			if (val.id === parseInt(req.params.eventid, 10)) {
-				eventDatabase.events.splice(val.id, 1);
+        return res.status(200).json({
+          Message: 'User deleted',
+          Error: false,
+          deleteOutput: val,
+        });
+      }
+    }
 
-				return res.status(200).json({
-					Message: `User deleted`,
-					Error: false,
-					deleteOutput: val
-				})
-			}
-		}
-
-		return res.status(409).json({
-			Message: 'User id not found',
-			Error: true
-		})
-
-	}
-
-
-
-
+    return res.status(409).json({
+      Message: 'User id not found',
+      Error: true,
+    });
+  }
 }
