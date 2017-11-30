cb81e2e5be333649fac1e731f4845bfaa272f1ef
albert85 <tmtalbert@gmail.com>
2017-11-30 16:13:48 +0100

    updates web-token-features

--- scripts/eventManager.js
+++ scripts/eventManager.js
@@ -1,13 +1,15 @@
-    document.getElementById('signInBtn').addEventListener("click",()=>{
-        // locate email input
-        let email = document.getElementById('inputEmail').value;
-        console.log(email);
-        // get password
-        let inputPassword = document.getElementById('inputPassword').value;
-        // validate email and password to login in
-        if(email === "admin@andela.com" && inputPassword === "admin"){
-            document.URL('Admin.html');
-        }else{
+document.getElementById('signInBtn').addEventListener('click', () => {
+  // locate email input
+  const email = document.getElementById('inputEmail').value;
+  //   alert(email);
+  // get password
+  const inputPassword = document.getElementById('inputPassword').value;
 
-        }
-    })
+
+  // validate email and password to login in
+  if (email !== 'admin@andela.com' && inputPassword !== 'admin' && email.length !== 0 && inputPassword.length !== 0) {
+    document.location.href = 'userpage.html';
+  } else if (email === 'admin@andela.com' && inputPassword === 'admin' && email.length !== 0 && inputPassword.length !== 0) {
+    document.location.href = 'Admin.html';
+  }
+});
