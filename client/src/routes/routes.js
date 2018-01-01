e072975180a0f2aeafa9c863e480356092b91727
albert85 <tmtpalbert@gmail.com>
2018-01-01 14:47:40 +0100

    create center react component

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-import Login from '../component/Home';
-import SignUp from '../component/signUpPage';
+import login from '../component/home';
+import signUp from '../component/signUpPage';
+import eventHomePage from '../component/eventHomePage';
+import centers from '../component/centers';
 
 class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route path="/signup" component={SignUp} />
+                    <Route exact path="/" component={login} />
+                    <Route path="/signup" component={signUp} />
+                    <Route path="/event-home-page" component={eventHomePage} />
+                    <Route path="/centers" component={centers} />
                     <Redirect to="/" />
                 </Switch>
             </BrowserRouter>
