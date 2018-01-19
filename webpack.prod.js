75b73e1aa0a2d46d3518f1cc357fe6e5da6488c7
albert85 <tmtpalbert@gmail.com>
2018-01-19 13:35:14 +0100

    deploy on heroku

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,12 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true,
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
